470e984165a4994628ae76623ef0753f27e63e63
Alex Blom <alex@alexblom.com>
2016-03-23 19:18:17 -0400

    fix(application route): wrong splash import path

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import SplashscreenMixin from 'ember-platform-cordova/mixins/splashscreen';
+import SplashscreenMixin from 'ember-cordova/mixins/splashscreen';
 
 const {
   Route
