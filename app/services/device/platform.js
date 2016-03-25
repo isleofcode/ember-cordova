d3ef7097be8911ca2bdc72eae713599c40ef3f7e
@anulman <anulman@gmail.com>
2016-03-24 20:24:23 -0400

    refactor(platform-service): tidy app file

--- app/services/device/platform.js
+++ app/services/device/platform.js
@@ -1,5 +1,1 @@
-/* jshint esnext:true */
-
-import PlatformService from 'ember-cordova/services/device/platform';
-
-export default PlatformService.extend({});
+export { default } from 'ember-cordova/services/device/platform';
