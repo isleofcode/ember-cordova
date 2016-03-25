3f5d0598fe9b66dfef3fd997bfc1a15ad4fabed4
Alex Blom <alex@alexblom.com>
2016-03-25 01:17:21 -0400

    fix(live-reload)

--- app/initializers/in-app-livereload.js
+++ app/initializers/in-app-livereload.js
@@ -1,19 +1,22 @@
 /* globals cordova */
 
+import Ember from 'ember';
 import config from '../config/environment';
-import reloadInitializer from 'ember-cordova/initializers/in-app-livereload';
 
-var inAppReload = reloadInitializer.initialize;
+const { run } = Ember;
 
-export var initialize = function(app) {
-  if(typeof cordova === 'undefined' ||
-      config.environment !== 'development' ||
-      (config.cordova &&
-        (!config.cordova.liveReload || !config.cordova.liveReload.enabled))) {
-    return;
+export function() {
+  var config = config.cordova;
+
+  if (config.cordova && config.cordova.liveReload === true) {
+    console.log("FIRING");
+    var url = config.cordova.reloadUrl;
+    if (window.location.href.indexOf('file://') > -1) {
+      run.later(function() {
+        window.location.replace(url);
+      }, 50);
+    }
   }
-
-  return inAppReload(app, config);
 };
 
 export default {
