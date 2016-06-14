cfaffe497b5a0f979becb96e473c6cbf7fc33dbf
Alex Blom <alex@alexblom.com>
2016-06-13 22:54:08 -0400

    tests(link-environment)

---
+++ node-tests/unit/tasks/link-environment-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Link Environment Task', () => {
+  xit('throws an error if project does not exist', () => {
+  });
+
+  xit('symlinks dist/ to ember-cordova/cordova/www', () => {
+  });
+});
