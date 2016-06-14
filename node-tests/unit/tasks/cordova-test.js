b1b232716adc56fc61aba3230b482a8f56abdf1d
Alex Blom <alex@alexblom.com>
2016-06-13 22:52:47 -0400

    tests(cordova)

---
+++ node-tests/unit/tasks/cordova-test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const td           = require('testdouble');
+const BashTask     = require('../../../lib/tasks/bash');
+const CordovaTask  = require('../../../lib/tasks/cordova');
+
+const mockProject  = require('../../fixtures/ember-cordova-mock/project');
+const isObject     = td.matchers.isA(Object);
+
+describe('Cordova cordovaCmd Task', () => {
+  let bashDouble, cordovaCmd;
+
+  beforeEach(() => {
+    bashDouble = td.replace(BashTask.prototype, 'runCommand');
+
+    cordovaCmd = new CordovaTask(mockProject);
+  });
+
+  afterEach(() => {
+    td.reset();
+  });
+
+  it('proxies the command to cordova', () => {
+    cordovaCmd.rawArgs = ['prepare'];
+    cordovaCmd.run();
+
+    td.verify(bashDouble('cordova prepare', isObject));
+  });
+
+  it('proxies multi argument commands', () => {
+    cordovaCmd.rawArgs = ['plugin add foo'];
+    cordovaCmd.run();
+
+    td.verify(bashDouble('cordova plugin add foo', isObject));
+  });
+});
+
