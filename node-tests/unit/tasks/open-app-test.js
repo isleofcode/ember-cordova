5283568db31e3f1cd4c14ffa36adeb476dc4bdd2
Alex Blom <alex@alexblom.com>
2016-06-15 18:31:24 -0400

    tests(open-app-task)

--- node-tests/unit/tasks/open-app-test.js
+++ node-tests/unit/tasks/open-app-test.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const td                = require('testdouble');
-const path              = require('path');
-const BashTask          = require('../../../lib/tasks/bash');
-const OpenAppTask       = require('../../../lib/tasks/open-app');
-const MockUI            = require('ember-cli/tests/helpers/mock-ui');
-const expect            = require('../../helpers/expect');
+const td            = require('testdouble');
+const path          = require('path');
+const BashTask      = require('../../../lib/tasks/bash');
+const OpenAppTask   = require('../../../lib/tasks/open-app');
+const MockUI        = require('ember-cli/tests/helpers/mock-ui');
+const expect        = require('../../helpers/expect');
 
-const mockProject    = require('../../fixtures/ember-cordova-mock/project');
-const isObject          = td.matchers.isA(Object);
+const mockProject   = require('../../fixtures/ember-cordova-mock/project');
+const isObject      = td.matchers.isA(Object);
 
 describe('Open App Task', () => {
   let bashDouble, openApp, cdvPath;
@@ -38,6 +38,10 @@ describe('Open App Task', () => {
     td.verify(bashDouble(expectedCmd, isObject));
   }),
 
-  xit('outputs an error if no platform is specified', () => {
+  it('outputs an error if no platform is specified', () => {
+    openApp.platform = 'invalidPlatform';
+    openApp.run();
+
+    expect(openApp.ui.output).to.contain('platform is not supported');
   });
 });
