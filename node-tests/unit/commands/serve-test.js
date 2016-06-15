bea7222a5deacf8c9201e064094d9a259deb8978
Alex Blom <alex@alexblom.com>
2016-06-15 18:36:12 -0400

    testing(serve-cmd)

---
+++ node-tests/unit/commands/serve-test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const ServeCmd      = require('../../../lib/commands/serve');
+
+describe('Prepare Command', () => {
+  afterEach(() => {
+    td.reset();
+  });
+
+  xit('is tested', () => {
+  });
+});
