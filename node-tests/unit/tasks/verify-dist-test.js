ce1eabc5c3a37bce0e50887a09ab26fb40de83dc
Alex Blom <alex@alexblom.com>
2016-06-15 18:30:13 -0400

    tests(verify-dist-task)

--- node-tests/unit/tasks/verify-dist-test.js
+++ node-tests/unit/tasks/verify-dist-test.js
@@ -1,31 +1,40 @@
 'use strict';
 
-const td             = require('testdouble');
-const fs             = require('fs');
-const path           = require('path');
-const expect         = require('../../helpers/expect');
-const VerifyDistTask = require('../../../lib/tasks/verify-dist');
-const assert = require('chai').assert;
+const td            = require('testdouble');
+const fs            = require('fs');
+const path          = require('path');
+const expect        = require('../../helpers/expect');
+const VerifyTask    = require('../../../lib/tasks/verify-dist');
+const assert        = require('chai').assert;
 
-const mockProject    = require('../../fixtures/ember-cordova-mock/project');
-const isObject       = td.matchers.isA(Object);
+const mockProject   = require('../../fixtures/ember-cordova-mock/project');
 
 describe('Verify Dist Task', () => {
-  let verify, mkDir;
+  let verify;
 
   beforeEach(() => {
-    td.replace(fs, 'mkdirSync');
-    verify = new VerifyDistTask(mockProject);
+    verify = new VerifyTask(mockProject);
   });
 
   afterEach(() => {
     td.reset();
   });
 
-  xit('resolves if dist exists', () => {
+  it('resolves if dist exists', () => {
+   td.replace(fs, 'existsSync', () => {
+     return true;
+   });
+
+   expect(verify.run()).to.be.fulfilled;
   });
 
   it('creates dist if it does not exist', () => {
+    td.replace(fs, 'mkdirSync');
+
+    td.replace(fs, 'existsSync', () => {
+      return false;
+    });
+
     verify.run();
 
     const expectedPath = path.resolve(__dirname, '..', '..', 'fixtures', 'ember-cordova-mock', 'dist');
