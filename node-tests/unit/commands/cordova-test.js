92f19317a96733877ec09701a5475a8f9b8204b2
Alex Blom <alex@alexblom.com>
2016-06-15 18:29:19 -0400

    tests(cordova-cmd)

---
+++ node-tests/unit/commands/cordova-test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const td            = require('testdouble');
+const expect        = require('../../helpers/expect');
+const Promise       = require('ember-cli/lib/ext/promise');
+
+const CordovaCmd    = require('../../../lib/commands/cordova');
+const BashTask      = require('../../../lib/tasks/bash');
+
+const mockProject   = require('../../fixtures/ember-cordova-mock/project');
+const isObject      = td.matchers.isA(Object);
+
+
+//TODO - kill Task
+describe('Cordova Command', () => {
+  beforeEach(() => {
+    CordovaCmd.ui = mockProject.ui;
+    CordovaCmd.project = mockProject.project;
+  });
+
+  afterEach(() => {
+    td.reset();
+  });
+
+  it('warns if an ember-cordova command is used', () => {
+    CordovaCmd.validateAndRun(['build']);
+    expect(CordovaCmd.ui.output).to.contain('bypassed ember-cordova command');
+  });
+
+  it('warns if cordova command is unknown', () => {
+    CordovaCmd.validateAndRun(['foo']);
+    expect(CordovaCmd.ui.output).to.contain('unknown Cordova command');
+  });
+
+  it('proxies the command to cordova', () => {
+    let bashDouble = td.replace(BashTask.prototype, 'runCommand');
+    CordovaCmd.run({}, ['prepare']);
+
+    td.verify(bashDouble('cordova prepare', isObject));
+  });
+
+  it('proxies multi argument commands', () => {
+    let bashDouble = td.replace(BashTask.prototype, 'runCommand');
+    CordovaCmd.run({}, ['plugin add foo']);
+
+    td.verify(bashDouble('cordova plugin add foo', isObject));
+  });
+});
