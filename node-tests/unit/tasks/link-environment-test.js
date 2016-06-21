150d7291034831ca740877753d0d1523c91a2fda
Alex Blom <alex@alexblom.com>
2016-06-20 23:17:10 -0400

    test(link-env-task)

--- node-tests/unit/tasks/link-environment-test.js
+++ node-tests/unit/tasks/link-environment-test.js
@@ -1,9 +1,41 @@
 'use strict';
 
+const td            = require('testdouble');
+const fs            = require('fs-extra');
+const BashTask      = require('../../../lib/tasks/bash');
+const LinkTask      = require('../../../lib/tasks/link-environment');
+const VerifyTask    = require('../../../lib/tasks/verify-dist');
+
+const mockProject   = require('../../fixtures/ember-cordova-mock/project');
+
 describe('Link Environment Task', () => {
-  xit('runs verify dist task', () => {
+  it('runs verify dist task', () => {
+    const verifyDouble = td.replace(VerifyTask.prototype, 'run');
+
+    const linkEnv = new LinkTask(mockProject);
+    linkEnv.run();
+
+    td.verify(verifyDouble());
   });
 
-  xit('symlinks dist/ to ember-cordova/cordova/www', () => {
+  it('undoes prior symlink', () => {
+    const linkEnv = new LinkTask(mockProject);
+    linkEnv.run();
+
+    td.replace(fs, 'removeSync', () => {
+    });
+
+    td.verify(fs.removeSync());
+  });
+
+  it('symlinks dist/ to ember-cordova/cordova/www', () => {
+    const linkEnv = new LinkTask(mockProject);
+    linkEnv.run();
+
+    td.replace(fs, 'ensureSymlinkSync', () => {
+      //no function here causes test to fail
+    });
+
+    td.verify(fs.ensureSymlinkSync());
   });
 });
