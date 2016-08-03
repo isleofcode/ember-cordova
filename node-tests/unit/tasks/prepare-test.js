7b1daf804bf61531950d6aa0141ac8b56a3d3937
Alex Blom <alex@alexblom.com>
2016-08-03 18:47:51 -0400

    tests(prepare-task)

--- node-tests/unit/tasks/prepare-test.js
+++ node-tests/unit/tasks/prepare-test.js
@@ -1,27 +1,32 @@
 'use strict';
 
 const td            = require('testdouble');
-const BashTask      = require('../../../lib/tasks/bash');
-const PrepareTask   = require('../../../lib/tasks/prepare');
-
 const mockProject   = require('../../fixtures/ember-cordova-mock/project');
 const isObject      = td.matchers.isA(Object);
 
+const setupPrepareTask = function() {
+  const PrepareTask = require('../../../lib/tasks/prepare');
+  return new PrepareTask(mockProject);
+};
+
 describe('Prepare Task', () => {
-  let bashDouble, prepare;
-
-  beforeEach(() => {
-    bashDouble = td.replace(BashTask.prototype, 'runCommand');
-
-    prepare = new PrepareTask(mockProject);
-  });
-
   afterEach(() => {
     td.reset();
   });
 
   it('runs cordova prepare', () => {
+    const cdvPrepare = td.replace('cordova-lib/src/cordova/prepare');
+    let prepare = setupPrepareTask();
+
     prepare.run();
-    td.verify(bashDouble('cordova prepare', isObject));
+    td.verify(cdvPrepare(isObject));
+  });
+
+  it('proxies via cordova run', () => {
+    const cordovaRun = td.replace('../../../lib/utils/cordova-run');
+    let prepare = setupPrepareTask();
+
+    prepare.run();
+    td.verify(cordovaRun(isObject, isObject, []));
   });
 });
