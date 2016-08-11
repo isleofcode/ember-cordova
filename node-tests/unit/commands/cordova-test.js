c6dc870eb41bcc008eed027bafac87f6fffe333a
Alex Blom <alex@alexblom.com>
2016-08-11 14:01:17 -0400

    test(cordova-command): stub verify so we dont fail travis

--- node-tests/unit/commands/cordova-test.js
+++ node-tests/unit/commands/cordova-test.js
@@ -1,54 +1,51 @@
 'use strict';
 
 const BashTask      = require('../../../lib/tasks/bash');
+const CordovaCmd    = require('../../../lib/commands/cordova');
+const VerifyInstall = require('../../../lib/tasks/verify-cordova-installed');
+
 const td            = require('testdouble');
 const expect        = require('../../helpers/expect');
 const Promise       = require('ember-cli/lib/ext/promise');
 
-td.replace('../../../lib/tasks/verify-cordova-installed', function() {
-  return {
-    run: function() {
-      return new Promise(function(resolve) {
-        resolve();
-      });
-    }
-  }
-});
-
 const mockProject   = require('../../fixtures/ember-cordova-mock/project');
 const isObject      = td.matchers.isA(Object);
 
-const setupCordovaCmd = function() {
-  const CordovaCmd = require('../../../lib/commands/cordova');
-
-  CordovaCmd.ui = mockProject.ui;
-  CordovaCmd.project = mockProject.project;
-  return CordovaCmd;
-};
-
 describe('Cordova Command', () => {
+  let cmd;
+
+  beforeEach(() => {
+    td.replace(VerifyInstall.prototype, 'run', () => {
+      return Promise.resolve();
+    });
+
+    CordovaCmd.ui = mockProject.ui;
+    CordovaCmd.project = mockProject.project;
+    cmd = CordovaCmd;
+  });
+
   afterEach(() => {
     td.reset();
   });
 
   it('warns if an ember-cordova command is used', () => {
-    let cmd = setupCordovaCmd();
     cmd.validateAndRun(['build']);
     expect(cmd.ui.output).to.contain('bypassed ember-cordova command');
   });
 
   it('warns if cordova command is unknown', () => {
-    let cmd = setupCordovaCmd();
     cmd.validateAndRun(['foo']);
     expect(cmd.ui.output).to.contain('unknown Cordova command');
   });
 
-  it('proxies argument commands', () => {
+  it('proxies argument commands', (done) => {
     const bashDouble = td.replace(BashTask.prototype, 'runCommand');
-    let cmd = setupCordovaCmd();
 
-    return cmd.validateAndRun(['plugin add foo']).then(() => {
+    cmd.validateAndRun(['plugin add foo']).then(() => {
       td.verify(bashDouble('cordova plugin add foo', isObject));
+      done();
+    }).catch(function(err) {
+      done(err);
     });
   });
 });
