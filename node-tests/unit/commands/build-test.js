9ca70f05d86ff7e39f144eb3d373fb6aa3d744b4
Alex Blom <alex@alexblom.com>
2016-09-14 22:47:00 -0400

    refactor(build-opts)

--- node-tests/unit/commands/build-test.js
+++ node-tests/unit/commands/build-test.js
@@ -6,41 +6,43 @@ var ui              = require('../../../lib/utils/ui');
 var expect          = require('../../helpers/expect');
 var Promise         = require('ember-cli/lib/ext/promise');
 
-var BuildCmd        = require('../../../lib/commands/build');
 var BuildTask       = require('../../../lib/tasks/ember-build');
 var CdvBuildTask    = require('../../../lib/tasks/cordova-build');
 var HookTask        = require('../../../lib/tasks/run-hook');
 var PlatformTask    = require('../../../lib/tasks/validate/platform');
 
-var mockProject   = require('../../fixtures/ember-cordova-mock/project');
-var mockAnalytics = require('../../fixtures/ember-cordova-mock/analytics');
-var parsePlatformOpts = require('../../../lib/utils/parse-platform-opts');
+var mockProject     = require('../../fixtures/ember-cordova-mock/project');
+var mockAnalytics   = require('../../fixtures/ember-cordova-mock/analytics');
+
+var isAnything      = td.matchers.anything;
+
+var setupBuild = function() {
+  var BuildCmd = require('../../../lib/commands/build');
+
+  var project = mockProject.project;
+  project.config = function() {
+    return {
+      locationType: 'hash'
+    };
+  }
+
+  var build = new BuildCmd({
+    project: project,
+    ui: mockProject.ui
+  });
+  build.analytics = mockAnalytics;
+
+  return build;
+};
 
 describe('Build Command', function() {
-  var build;
-
-  beforeEach(function() {
-    var project = mockProject.project;
-    project.config = function() {
-      return {
-        locationType: 'hash'
-      };
-    }
-
-    build = new BuildCmd({
-      project: project,
-      ui: mockProject.ui
-    });
-    build.analytics = mockAnalytics;
-  });
-
   afterEach(function() {
     td.reset();
   });
 
   context('when locationType is hash', function() {
     var tasks;
-    var cordovaOptions;
+    var cordovaPlatform;
 
     beforeEach(function() {
       mockTasks();
@@ -63,8 +65,8 @@ describe('Build Command', function() {
         return Promise.resolve();
       });
 
-      td.replace(CdvBuildTask.prototype, 'run', function(_cordovaOptions) {
-        cordovaOptions = _cordovaOptions;
+      td.replace(CdvBuildTask.prototype, 'run', function() {
+        cordovaPlatform = this.platform;
 
         tasks.push('cordova-build');
         return Promise.resolve();
@@ -72,12 +74,16 @@ describe('Build Command', function() {
     }
 
     it('exits cleanly', function() {
+      var build = setupBuild();
+
       return expect(function() {
         build.run({});
       }).not.to.throw(Error);
     });
 
     it('runs tasks in the correct order', function() {
+      var build = setupBuild();
+
       return build.run({})
         .then(function() {
           //h-t ember-electron for the pattern
@@ -90,194 +96,27 @@ describe('Build Command', function() {
         });
     });
 
-    context('when passedPlatform is ios', function() {
-      it('platform eq ios', function() {
-        var passedPlatform = 'ios';
+    it('parses cordova build opts', function() {
+      var optDouble = td.replace('../../../lib/utils/parse-cordova-build-opts');
+      var build = setupBuild();
 
-        return build.run({
-          platform: passedPlatform
-        }).then(function() {
-          expect(cordovaOptions.platform).to.equal(passedPlatform);
-        });
-      });
-    });
-    context('when passedPlatform is android', function() {
-      it('platform eq android', function() {
-        var passedPlatform = 'android';
-
-        return build.run({
-          platform: passedPlatform
-        }).then(function() {
-          expect(cordovaOptions.platform).to.equal(passedPlatform);
-        });
+      return build.run({}).then(function() {
+        td.verify(optDouble(isAnything(), isAnything()));
       });
     });
 
-    describe('isRelease', function() {
-      context('when release is false', function() {
-        it('isRelease eq false', function() {
-          var passedPlatform = 'ios';
-          var passedRelease = false;
+    it('passes platform to cordova build task', function() {
+      var passedPlatform = 'android';
+      var build = setupBuild();
 
-          return build.run({
-            platform: passedPlatform,
-            isRelease: passedRelease,
-          }).then(function() {
-            expect(cordovaOptions.isRelease).to.equal(passedRelease);
-          });
-        });
-      });
-      context('when release is true', function() {
-        it('isRelease eq true', function() {
-          var passedPlatform = 'ios';
-          var passedRelease = true;
-
-          return build.run({
-            platform: passedPlatform,
-            isRelease: passedRelease,
-          }).then(function() {
-            expect(cordovaOptions.isRelease).to.equal(passedRelease);
-          });
-        });
-      });
-    });
-
-    describe('isEmulator', function() {
-      context('when device is false', function() {
-        it('isEmulator eq true', function() {
-          var passedPlatform = 'ios';
-          var passedDevice = false;
-
-          return build.run({
-            platform: passedPlatform,
-            isEmulator: passedDevice,
-          }).then(function() {
-            expect(cordovaOptions.isEmulator).to.equal(passedDevice);
-          });
-        });
-      });
-      context('when device is true', function() {
-        it('isEmulator eq false', function() {
-          var passedPlatform = 'ios';
-          var passedDevice = true;
-
-          return build.run({
-            platform: passedPlatform,
-            isEmulator: passedDevice,
-          }).then(function() {
-            expect(cordovaOptions.isEmulator).to.equal(passedDevice);
-          });
-        });
-      });
-    });
-
-    describe('buildConfig', function() {
-      context('when not passed', function() {
-        it('does not append buildConfig to options', function() {
-          var passedPlatform = 'ios';
-          var passedBuildConfig = undefined;
-
-          return build.run({
-            platform: passedPlatform,
-          }).then(function() {
-            expect(cordovaOptions.buildConfig).to.equal(passedBuildConfig);
-          });
-        });
-      });
-      context('when buildConfig is passed', function() {
-        it('appends buildConfig options to options', function() {
-          var passedPlatform = 'ios';
-          var passedBuildConfig = '/foo';
-
-          return build.run({
-            platform: passedPlatform,
-            buildConfig: passedBuildConfig,
-          }).then(function() {
-            expect(cordovaOptions.buildConfig).to.equal(passedBuildConfig);
-          });
-        });
-      });
-    });
-
-    describe('platformOpts', function() {
-      context('when platform is ios', function() {
-        it('passes ios options to CdvBuildTask', function()  {
-          var passedPlatform = 'ios';
-          var platformOpts = parsePlatformOpts(
-            cordovaOptions.platform,
-            cordovaOptions
-          );
-
-          return build.run({
-            platform: passedPlatform,
-            platformOpts: platformOpts
-          }).then(function() {
-            expect('codeSignIdentity' in cordovaOptions.platformOpts)
-            .to.equal('codeSignIdentity' in platformOpts);
-          });
-        });
-
-        it('filters out android options', function() {
-          var passedPlatform = 'ios';
-          var platformOpts = parsePlatformOpts(
-            cordovaOptions.platform,
-            cordovaOptions
-          );
-
-          return build.run({
-            platform: passedPlatform,
-            platformOpts: platformOpts
-          }).then(function() {
-            expect('alias' in cordovaOptions.platformOpts)
-            .to.equal('alias' in platformOpts);
-          });
-        });
-      });
-
-      context('when platform is android', function() {
-        it('passes android options to CdvBuildTask', function() {
-          var passedPlatform = 'android';
-          var platformOpts = parsePlatformOpts(
-            cordovaOptions.platform,
-            cordovaOptions
-          );
-
-          return build.run({
-            platform: passedPlatform,
-            platformOpts: platformOpts
-          }).then(function() {
-            expect('alias' in cordovaOptions.platformOpts)
-            .to.equal('alias' in platformOpts);
-          });
-        });
-
-        it('filters out ios options', function() {
-          var passedPlatform = 'android';
-          var platformOpts = parsePlatformOpts(
-            cordovaOptions.platform,
-            cordovaOptions
-          );
-
-          return build.run({
-            platform: passedPlatform,
-            platformOpts: platformOpts
-          }).then(function() {
-            expect('codeSignIdentity' in cordovaOptions.platformOpts)
-            .to.equal('codeSignIdentity' in platformOpts);
-          });
-        });
+      return build.run({platform: passedPlatform}).then(function() {
+        expect(cordovaPlatform).to.equal(passedPlatform);
       });
     });
   });
 
   context('when locationType is not hash', function() {
     beforeEach(function() {
-      build.project.config = function() {
-        return {
-          locationType: 'auto'
-        };
-      };
-
       td.replace(ui, 'writeLine',  function() {
         throw new Error('Exit Called');
       });
@@ -285,6 +124,13 @@ describe('Build Command', function() {
     });
 
     it('throws', function() {
+      var build = setupBuild();
+      build.project.config = function() {
+        return {
+          locationType: 'auto'
+        };
+      };
+
       return expect(function() {
         build.run({});
       }).to.throw(Error);
--- node-tests/unit/tasks/cordova-build-test.js
+++ node-tests/unit/tasks/cordova-build-test.js
@@ -6,195 +6,29 @@ var CdvRawTask      = require('../../../lib/tasks/cordova-raw');
 
 var setupBuildTask = function() {
   var CdvBuildTask = require('../../../lib/tasks/cordova-build');
-  return new CdvBuildTask({
-    project: mockProject.project,
-    ui: mockProject.ui,
-    platform: 'ios'
-  });
+  return new CdvBuildTask(mockProject);
 };
 
 describe('Cordova Build Task', function() {
-  var cdvBuild, build;
-
-  beforeEach(function() {
-    cdvBuild = td.replace(CdvRawTask.prototype, 'run');
-    build = setupBuildTask();
-  });
-
   afterEach(function() {
     td.reset();
   });
 
-  describe('platform', function() {
-    var DEFAULT_OPTS = ['--debug', '--emulator'];
-
-    context('when ios', function() {
-      it('creates a raw ios build task', function() {
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: ['ios'],
-          options: DEFAULT_OPTS
-        }));
-      });
-    });
-
-    context('when android', function() {
-      it('sets platform to android', function() {
-        build.platform = 'android';
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: ['android'],
-          options: DEFAULT_OPTS
-        }));
-      });
-    });
-  });
-
-  describe('isRelease', function() {
-    var DEFAULT_PLATFORMS = ['ios'];
-    var BASE_OPTIONS = ['--emulator'];
-
-    context('when not explicitly defined', function() {
-      it('passes --debug', function() {
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: ['--debug'].concat(BASE_OPTIONS)
-        }));
-      });
-    });
-
-    context('when true', function() {
-      it('passes --release', function() {
-        build.isRelease = true;
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: ['--release'].concat(BASE_OPTIONS)
-        }));
-      });
-    });
-
-    context('when false', function() {
-      it('passes --debug', function() {
-        build.isRelease = false;
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: ['--debug'].concat(BASE_OPTIONS)
-        }));
-      });
-    });
-  });
-
-  describe('isEmulator', function() {
-    var DEFAULT_PLATFORMS = ['ios'];
-    var DEFAULT_OPTS = ['--debug'];
-
-    context('when not explicitly defined', function() {
-      it('passes --emulator', function() {
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: DEFAULT_OPTS.concat('--emulator')
-        }));
-      });
-    });
-
-    context('when true', function() {
-      it('passes --device', function() {
-        build.isEmulator = true;
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: DEFAULT_OPTS.concat('--emulator')
-        }));
-      });
-    });
-
-    context('when false', function() {
-      it('passes --emulator', function() {
-        build.isEmulator = false;
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: DEFAULT_OPTS.concat('--device')
-        }));
-      });
-    });
-  });
-
-  describe('buildConfig', function() {
-    var DEFAULT_PLATFORMS = ['ios'];
-    var DEFAULT_OPTS = ['--debug', '--emulator'];
-
-    context('when not passed', function() {
-      it('does not pass buildConfig', function() {
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: DEFAULT_OPTS
-        }));
-      });
-    });
-
-    context('when passed', function() {
-      it('passes --buildConfig', function() {
-        build.buildConfig = 'foo';
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: DEFAULT_OPTS.concat('--buildConfig=foo')
-        }));
-      });
-    });
-  });
-
-  describe('platformOpts', function() {
-    var DEFAULT_PLATFORMS = ['ios'];
-    var DEFAULT_OPTS = ['--debug', '--emulator'];
-
-    context('when not passed', function() {
-      it('does not pass platformOpts', function() {
-        build.run();
-
-        td.verify(cdvBuild({
-          platforms: DEFAULT_PLATFORMS,
-          options: DEFAULT_OPTS
-        }));
-      });
-    });
-
-    context('when key values are undefined', function() {
-      it('does not pass platformOpts', function() {
-
-      });
-    });
-
-    context('when key values are defined', function() {
-      it('does not pass platformOpts', function() {
-
-      });
-    });
-  });
-
-  it('sets device flag when passed', function() {
-    build.isEmulator = false;
+  it('creates a raw build task', function() {
+    var cdvBuild = td.replace(CdvRawTask.prototype, 'run');
+    var build = setupBuildTask();
+    build.platform = 'ios';
     build.run();
 
-    td.verify(cdvBuild({
-      platforms: ['ios'],
-      options: ['--debug', '--device']
-    }));
+    td.verify(cdvBuild({platforms: ['ios'], options: []}));
+  });
+
+  it('sets platform to android', function() {
+    var cdvBuild = td.replace(CdvRawTask.prototype, 'run');
+    var build = setupBuildTask();
+    build.platform = 'android';
+    build.run();
+
+    td.verify(cdvBuild({platforms: ['android'], options: []}));
   });
 });
