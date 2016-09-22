a0f8ec944f26ba0f96b57ce33989c792d9390376
Alex Blom <alex@alexblom.com>
2016-09-22 13:25:28 -0400

    test(gitkeep-task)

--- node-tests/unit/tasks/update-gitignore-test.js
+++ node-tests/unit/tasks/update-gitignore-test.js
@@ -19,11 +19,34 @@ describe('Update gitignore Task', function() {
   };
 
   it('attempts to write ignore data to .gitignore', function() {
+    td.replace(fs, 'closeSync');
+    td.replace(fs, 'openSync');
+
+    var expectedGitkeep = '\n' +
+      'ember-cordova/cordova/www/*\n' +
+      '!ember-cordova/cordova/www/.gitkeep\n' +
+      'ember-cordova/cordova/plugins/*\n' +
+      '!ember-cordova/cordova/plugins/.gitkeep\n' +
+      'ember-cordova/cordova/platforms/*\n' +
+      '!ember-cordova/cordova/platforms/.gitkeep\n'
+
     var appendDouble = td.replace(fs, 'appendFileSync');
     var task = createTask();
     task.run();
 
-    td.verify(appendDouble('.gitignore', task.ignoreContent));
+    td.verify(appendDouble('.gitignore', expectedGitkeep));
+  });
+
+  it('stubs empty gitkeeps', function() {
+    var openDouble = td.replace(fs, 'openSync');
+    td.replace(fs, 'closeSync');
+
+    var task = createTask();
+    task.run();
+
+    td.verify(openDouble('ember-cordova/cordova/www/.gitkeep', 'w'));
+    td.verify(openDouble('ember-cordova/cordova/platforms/.gitkeep', 'w'));
+    td.verify(openDouble('ember-cordova/cordova/plugins/.gitkeep', 'w'));
   });
 
   it('outputs an error message and resolves if write fails', function() {
