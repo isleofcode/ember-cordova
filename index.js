e9690c44bd714d3f1808d7f5ebf69968ee9b501d
Alex Blom <alex@alexblom.com>
2016-11-10 00:17:58 -0500

    fix(asset-fingerprinting): dont dual fingerprint files, only include files for livereload

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 
 var commands              = require('./lib/commands');
 var getNetworkIp          = require('./lib/utils/get-network-ip');
-var getPlatformAssets     = require('./lib/utils/get-platform-assets');
+var cordovaPath           = require('./lib/utils/cordova-path');
+var cordovaAssets         = require('./lib/utils/cordova-assets');
 
-var existsSync            = require('fs').existsSync;
-var path                  = require('path');
-var chalk                 = require('chalk');
 var mergeTrees            = require('broccoli-merge-trees');
 var Funnel                = require('broccoli-funnel');
 
@@ -23,11 +21,17 @@ module.exports = {
     return commands;
   },
 
-  cordovaAssetTree: function(tree, assets) {
-    var pluginsTree = new Funnel(this.treeGenerator(assets.path), {
-      srcDir:  '/',
-      include: assets.files,
-      destDir: '/'
+  //In Livereload scenarios, we need to manually include cordova assets
+  cordovaAssetTree: function(tree) {
+    var platform = this.project.CORDOVA_PLATFORM;
+    var projectPath = cordovaPath(this.project);
+    var assets = cordovaAssets.getPaths(platform, projectPath);
+
+    cordovaAssets.validatePaths(assets.assetsPath, projectPath, this.ui);
+
+    var pluginsTree = new Funnel('ember-cordova/cordova', {
+      srcDir:  assets.assetsPath,
+      include: assets.files
     });
 
     return mergeTrees([tree, pluginsTree]);
@@ -36,24 +40,8 @@ module.exports = {
   treeForPublic: function() {
     var tree = this._super.treeForPublic.apply(this, arguments);
 
-    if (this.project.targetIsCordova) {
-      var platformAssets = getPlatformAssets(this.project);
-
-      if (platformAssets.path === undefined) {
-        throw new Error('ember-cordova: Did not receive platform asset path, canot not build');
-      };
-
-      var platformPath = path.join(platformAssets.path, 'cordova.js');
-      var pluginPath = path.join(platformAssets.path, 'cordova_plugins.js');
-      if (!existsSync(platformPath) || !existsSync(pluginPath)) {
-        this.ui.writeLine(chalk.yellow(
-          'WARNING: ember-cordova: Did not find cordova.js or cordova_plugins.js at ' +
-          platformAssets.path +
-          '. Ember App LiveReload will still work, but device & plugin APIS will fail.'
-        ));
-      }
-
-      return this.cordovaAssetTree(tree, platformAssets);
+    if (this.project.targetIsCordovaLivereload) {
+      return this.cordovaAssetTree(tree);
     }
 
     return tree;
