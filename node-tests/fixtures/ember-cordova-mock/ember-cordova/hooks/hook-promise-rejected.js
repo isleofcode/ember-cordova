98a89a7c02e1f7ff502e26d9f828f122662817b2
Reid Beels <reid@thedyrt.com>
2017-01-05 11:57:37 -0800

    fix(run-hook-test): Simplify promise returns from mock hooks

--- node-tests/fixtures/ember-cordova-mock/ember-cordova/hooks/hook-promise-rejected.js
+++ node-tests/fixtures/ember-cordova-mock/ember-cordova/hooks/hook-promise-rejected.js
@@ -1,7 +1,5 @@
 var Promise = require('ember-cli/lib/ext/promise');
 
 module.exports = function() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(5, () => reject('hook rejected'));
-  });
+  return Promise.reject('hook rejected');
 };
