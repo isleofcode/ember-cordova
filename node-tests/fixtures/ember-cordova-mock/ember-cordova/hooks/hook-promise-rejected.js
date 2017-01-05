9953959ef2f07bb1f84fef9b2e9418b16ac0d0f4
Reid Beels <reid@thedyrt.com>
2017-01-05 11:47:58 -0800

    fix(run-hook-test): Chai promise expectations need to be returned
    This fixes the expectations in run-hook-test so they actually trigger.
    One test is now failing, but I think the expectation it lays out is incorrect.

--- node-tests/fixtures/ember-cordova-mock/ember-cordova/hooks/hook-promise-rejected.js
+++ node-tests/fixtures/ember-cordova-mock/ember-cordova/hooks/hook-promise-rejected.js
@@ -1,7 +1,7 @@
 var Promise = require('ember-cli/lib/ext/promise');
 
 module.exports = function() {
-  return Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     setTimeout(5, () => reject('hook rejected'));
   });
 };
