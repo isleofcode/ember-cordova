7f526e3d4d3e3db619642d4f438d03244025dd9f
chundabear <bear.chunda@gmail.com>
2017-03-23 10:27:27 -0500

    feat(test) condense fns and add test stubs

---
+++ node-tests/unit/utils/edit-xml-test.js
@@ -0,0 +1,25 @@
+var editXml         = require('../../../lib/utils/edit-xml');
+var mockProject     = require('../../fixtures/ember-cordova-mock/project');
+var td              = require('testdouble');
+
+describe('Edit XML Util', function() {
+  describe('addNavigation function', function() {
+    it('add an allow-navigation node to the xml file', function() {
+      // add expect
+    });
+  });
+
+  describe('removeNavigation function', function() {
+    describe('if nodes placed by util exist', function() {
+      it('removes nodes', function() {
+        // add expect
+      });
+    });
+
+    describe('if client nodes exist', function() {
+      it('leaves client nodes', function() {
+        // add expect
+      });
+    });
+  });
+});
