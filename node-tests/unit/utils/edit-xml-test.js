9c08afad3008128ffcccd834bc7104ab75d8cd07
chundabear <bear.chunda@gmail.com>
2017-03-23 15:18:38 -0500

    feat(edit-xml-testing) add tests for util

--- node-tests/unit/utils/edit-xml-test.js
+++ node-tests/unit/utils/edit-xml-test.js
@@ -1,24 +1,50 @@
+var cordovaPath     = require('../../../lib/utils/cordova-path');
 var editXml         = require('../../../lib/utils/edit-xml');
+var expect          = require('../../helpers/expect');
 var mockProject     = require('../../fixtures/ember-cordova-mock/project');
-var td              = require('testdouble');
+var parseXml        = require('../../../lib/utils/parse-xml');
+var path            = require('path');
 
 describe('Edit XML Util', function() {
+  beforeEach(function() {
+    editXml.removeNavigation(mockProject.project);
+  });
+
+  var totalNavNodes;
+  var host = 'http://localhost:8080';
+
   describe('addNavigation function', function() {
-    it('add an allow-navigation node to the xml file', function() {
-      // add expect
+    beforeEach(function() {
+      editXml.addNavigation(mockProject.project, host);
+    });
+
+    it('add node to the xml file in addition to client nodes', function() {
+      var cdvPath = cordovaPath(mockProject.project);
+      var configPath = path.join(cdvPath, 'config.xml');
+
+      parseXml(configPath).then(function(json) {
+        totalNavNodes = json.widget['allow-navigation'];
+        expect(totalNavNodes).to.equal(3);
+      });
     });
   });
 
   describe('removeNavigation function', function() {
     describe('if nodes placed by util exist', function() {
-      it('removes nodes', function() {
-        // add expect
-      });
-    });
+      it('removes util placed nodes and keep client nodes', function() {
+        beforeEach(function() {
+          editXml.addNavigation(mockProject.project, host);
+        });
 
-    describe('if client nodes exist', function() {
-      it('leaves client nodes', function() {
-        // add expect
+        editXml.removeNavigation(mockProject.project);
+
+        var cdvPath = cordovaPath(mockProject.project);
+        var configPath = path.join(cdvPath, 'config.xml');
+
+        parseXml(configPath).then(function(json) {
+          totalNavNodes = json.widget['allow-navigation'];
+          expect(totalNavNodes).to.equal(2);
+        });
       });
     });
   });
