7b7a920b3db3397b444308c7eeaaa2ea12d2ea3b
Jordan Yee <jordanyee@users.noreply.github.com>
2017-03-23 23:28:56 -0400

    feat(make-icons): Throw error if no added platforms and platform option is `added`

--- node-tests/unit/commands/make-icons-test.js
+++ node-tests/unit/commands/make-icons-test.js
@@ -20,17 +20,6 @@ describe('Make Icons Command', function() {
 
       return Promise.resolve();
     });
-
-    td.replace('../../../lib/utils/get-added-platforms', function() {
-      return addedPlatforms;
-    });
-
-    MakeIconsCmd = require('../../../lib/commands/make-icons');
-
-    makeIcons = new MakeIconsCmd({
-      project: mockProject.project,
-      analytics: mockAnalytics
-    });
   });
 
   afterEach(function() {
@@ -39,39 +28,80 @@ describe('Make Icons Command', function() {
     td.reset();
   });
 
-  /* eslint-disable max-len */
-  context('when options and platform is `added`', function() {
-    var options = {
-      source: 'ember-cordova/icon.svg',
-      platform: ['added']
-    };
-
+  context('when added platforms', function() {
     beforeEach(function() {
-      return makeIcons.run(options);
+      td.replace('../../../lib/utils/get-added-platforms', function() {
+        return addedPlatforms;
+      });
+
+      MakeIconsCmd = require('../../../lib/commands/make-icons');
+
+      makeIcons = new MakeIconsCmd({
+        project: mockProject.project,
+        analytics: mockAnalytics
+      });
     });
 
-    it('calls icon task with passed source, added platforms, and projectPath', function() {
-      expect(iconTaskOptions.source).to.equal(options.source);
-      expect(iconTaskOptions.platforms).to.deep.equal(addedPlatforms);
-      expect(iconTaskOptions.projectPath).to.equal('ember-cordova/cordova');
+    /* eslint-disable max-len */
+    context('when options and platform is `added`', function() {
+      var options = {
+        source: 'ember-cordova/icon.svg',
+        platform: ['added']
+      };
+
+      beforeEach(function() {
+        return makeIcons.run(options);
+      });
+
+      it('calls icon task with passed source, added platforms, and projectPath', function() {
+        expect(iconTaskOptions.source).to.equal(options.source);
+        expect(iconTaskOptions.platforms).to.deep.equal(addedPlatforms);
+        expect(iconTaskOptions.projectPath).to.equal('ember-cordova/cordova');
+      });
     });
+
+    context('when options and platform is not `added`', function() {
+      var options = {
+        source: 'ember-cordova/icon.svg',
+        platform: ['ios']
+      };
+
+      beforeEach(function() {
+        return makeIcons.run(options);
+      });
+
+      it('calls icon task with passed source, passed platform, and projectPath', function() {
+        expect(iconTaskOptions.source).to.equal(options.source);
+        expect(iconTaskOptions.platforms).to.equal(options.platform);
+        expect(iconTaskOptions.projectPath).to.equal('ember-cordova/cordova');
+      });
+    });
+    /* eslint-enable max-len */
   });
 
-  context('when options and platform is not `added`', function() {
-    var options = {
-      source: 'ember-cordova/icon.svg',
-      platform: ['ios']
-    };
-
+  context('when no added platforms', function() {
     beforeEach(function() {
-      return makeIcons.run(options);
+      td.replace('../../../lib/utils/get-added-platforms', function() {
+        return [];
+      });
+
+      MakeIconsCmd = require('../../../lib/commands/make-icons');
+
+      makeIcons = new MakeIconsCmd({
+        project: mockProject.project,
+        analytics: mockAnalytics
+      });
     });
 
-    it('calls icon task with passed source, passed platform, and projectPath', function() {
-      expect(iconTaskOptions.source).to.equal(options.source);
-      expect(iconTaskOptions.platforms).to.equal(options.platform);
-      expect(iconTaskOptions.projectPath).to.equal('ember-cordova/cordova');
+    context('when options and platform is `added`', function() {
+      var options = {
+        source: 'ember-cordova/icon.svg',
+        platform: ['added']
+      };
+
+      it('throws an error', function() {
+        expect(function() { makeIcons.run(options) }).to.throw(Error);
+      });
     });
   });
-  /* eslint-enable max-len */
 });
