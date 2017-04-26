d90de4e955ef9d6c79d1ae771af304619ff4271d
Alex Blom <alex@alexblom.com>
2017-04-26 01:04:40 -0400

    test(parse-cordova-build-opts): test new behaviour

--- node-tests/unit/utils/parse-cordova-build-opts-test.js
+++ node-tests/unit/utils/parse-cordova-build-opts-test.js
@@ -4,32 +4,13 @@ var expect        = require('../../helpers/expect');
 var parseCordovaOpts = require('../../../lib/utils/parse-cordova-build-opts');
 
 describe('Parse Cordova Build Options Util', function() {
-  it('sets default debug/device flags', function() {
-    var cordovaOpts = parseCordovaOpts('ios', {})
-    expect(cordovaOpts).to.deep.equal([
-      '--debug',
-      '--device'
-    ]);
-  });
+  it('sets generic flags', function() {
+    var cordovaOpts = parseCordovaOpts('ios', {
+      release: true, device: 'deviceName'
+    })
 
-  it('sets emulator flag when passed', function() {
-    var options = { emulator: true };
-    var cordovaOpts = parseCordovaOpts('ios', options)
-
-    expect(cordovaOpts).to.deep.equal([
-      '--debug',
-      '--emulator'
-    ]);
-  });
-
-  it('sets release flag when passed', function() {
-    var options = { release: true };
-    var cordovaOpts = parseCordovaOpts('ios', options)
-
-    expect(cordovaOpts).to.deep.equal([
-      '--release',
-      '--device'
-    ]);
+    expect(cordovaOpts.release).to.equal(true);
+    expect(cordovaOpts.device).to.equal('deviceName');
   });
 
   describe('for ios', function() {
@@ -37,8 +18,14 @@ describe('Parse Cordova Build Options Util', function() {
       var options = { codeSignIdentity: 'FOO' };
       var cordovaOpts = parseCordovaOpts('ios', options)
 
-      expect(cordovaOpts).to.contain('--codeSignIdentity=FOO');
-      expect(cordovaOpts.length).to.equal(3);
+      expect(cordovaOpts.codeSignIdentity).to.equal('FOO');
+    });
+
+    it ('does not parse android options', function() {
+      var options = { keystore: 'FOO-KEYSTORE' };
+      var cordovaOpts = parseCordovaOpts('ios', options)
+
+      expect(cordovaOpts.keystore).to.equal(undefined);
     });
   });
 
@@ -47,8 +34,14 @@ describe('Parse Cordova Build Options Util', function() {
       var options = { keystore: 'FOO-KEYSTORE' };
       var cordovaOpts = parseCordovaOpts('android', options)
 
-      expect(cordovaOpts).to.contain('--keystore=FOO-KEYSTORE');
-      expect(cordovaOpts.length).to.equal(3);
+      expect(cordovaOpts.keystore).to.equal('FOO-KEYSTORE');
+    });
+
+    it('does not parse ios options', function() {
+      var options = { codeSignIdentity: 'FOO' };
+      var cordovaOpts = parseCordovaOpts('android', options)
+
+      expect(cordovaOpts.codeSignIdentity).to.equal(undefined);
     });
   });
 });
